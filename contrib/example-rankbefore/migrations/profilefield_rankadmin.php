4657b6c74f9e2de2a914b1bdf8140309e9336010
javiexin <javiexin@gmail.com>
2015-07-17 20:30:07 +0200

    Example: Multi Ranks (before and after)

---
+++ contrib/example-rankbefore/migrations/profilefield_rankadmin.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ *
+ * Advanced Profile Fields Pack - Admin Rank example
+ *
+ * @copyright (c) 2015 javiexin ( www.exincastillos.es )
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ * @author Javier Lopez (javiexin)
+ */
+
+namespace javiexin\advancedpf\migrations;
+
+class profilefield_rankadmin extends \phpbb\db\migration\profilefield_base_migration
+{
+	static public function depends_on()
+	{
+		return array(
+			'\javiexin\advancedpf\migrations\profilefield_individual',
+		);
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('custom', array(array($this, 'create_custom_field'))),
+		);
+	}
+
+	protected $profilefield_name = 'jx_rank_admin';
+
+	protected $profilefield_database_type = array('VCHAR', '');
+
+	protected $profilefield_data = array(
+		'field_name'			=> 'jx_rank_admin',
+		'field_type'			=> 'javiexin.advancedpf.profilefields.type.imgsel',
+		'field_ident'			=> 'jx_rank_admin',
+		'field_length'			=> '0',
+		'field_minlen'			=> '0',
+		'field_maxlen'			=> '0',
+		'field_novalue'			=> '',
+		'field_default_value'	=> '',
+		'field_validation'		=> 'images/ranks',
+		'field_required'		=> 0,
+		'field_show_novalue'	=> 0,
+		'field_show_on_reg'		=> 0,
+		'field_show_on_pm'		=> 0,
+		'field_show_on_vt'		=> 1,
+		'field_show_on_ml'		=> 1,
+		'field_show_profile'	=> 0,
+		'field_hide'			=> 0,
+		'field_no_view'			=> 0,
+		'field_active'			=> 1,
+		'field_individual'		=> 1,
+		'field_is_contact'		=> 0,
+		'field_contact_desc'	=> '',
+		'field_contact_url'		=> '',
+	);
+}
---
+++ contrib/example-rankbefore/readme.txt
@@ -0,0 +1,25 @@
+EXAMPLE - ADMIN RANK
+=====================
+
+In this example, it is demonstrated how to use this extension to add a second rank to your profile, and show it in 
+most relevant places.
+
+This is intended to be positioned BEFORE the standard rank, and it loads any of the images in the images/ranks folder
+as additional ranks.  I have tested this with the "Star Ranks 2.0.3" rank set downloaded from here:
+https://www.phpbb.com/customise/db/style/phpbb3_star_ranks/
+The reason is that it has multiple rank images, both for regular ranks, administrative ranks and special ranks.
+
+These images are presented in most possible places where Profile Fields are shown: in the Member Profile, in the Memberlist,
+and in the Viewtopic mini profile.  They are NOT shown in the Private Message mini profile.
+
+These elements are positioned in new standard event places, that correspond to new events that have been requested for 3.1.6,
+and that are included in the changes performed to test this extension.  No CSS and no JavaScript is used here.  You may need
+to adapt this to fit your needs.
+
+To use this example in your forum, simply copy all the contents of this folder to the root of the extension folder, disable and
+re-enable the extension (to execute the migration file that installs the profile field), and then configure a few profiles
+with the new additional rank.
+
+To remove it, if you do not want to continue to use it, you will have to remove the created Profile Field ("jx_rank_admin"), 
+and remove the files that you have copied.
+
